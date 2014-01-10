5805412ea451512745bb43f3e677a2306bb42e92
Li <vjzkbb@gmail.com>
2014-01-10 21:16:53 +0400

    box2&box3

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,6 +4,7 @@
             <span class="big-tit">Уютные коттеджи</span>
             <span class="mini-tit"> на заповедном берегу Волги</span>
         </p>
+
         <div class="act-price">
             <span class="active">1100 р</span> <span class="no-price">вместо</span> <span class="no-price"><hr>2200p</span>
         </div>
@@ -13,21 +14,26 @@
         <div class="window">
             <div class="w_info">
                 <p>Подайте заявку прямо</p>
+
                 <p>сейчас и получите скидку</p>
+
                 <p>50% на вторые сутки!</p>
             </div>
             <input type="text" class="default-field" placeholder="Ваше имя"/>
             <input type="text" class="default-field" placeholder="*Ваш телефон"/>
+
             <p class="inp_descr">*Ваши данные в безопасности</p>
             <input type="text" class="default-field" placeholder="Дата бронирования"/>
-            <p class="inp_descr">C вами свяжется наш менеджер<br> 
+
+            <p class="inp_descr">C вами свяжется наш менеджер<br>
                 в течении 15ти минут</p>
+
             <div class="button reservd"></div>
         </div>
+        <div class="anchor"></div>
     </article>
     <article class="in_scrollspy about" id="test_2">
         <div class="centrator">
-            <!--            <div class="picterself"></div>-->
             <h1>О базе отдыха Илеть</h1>
             <ul class="abt-ul">
                 <li>
@@ -36,32 +42,48 @@
                         волейбольная площадка и футбольное поле!</p>
                 </li>
                 <li>
-                    <div class="abt-icon abt-icon-2"></div>
-                    <p>На территории базы имеется  детская игровая зона.</p>
-                </li>
-                <li>
-                    <div class="abt-icon abt-icon-3"></div>
-                    <p>А также на нашей базе  соверешенно бесплатная охраняемая стоянка.</p>
-                </li>
-                <li>
                     <div class="abt-icon abt-icon-4"></div>
                     <p>Дома полностью отапливаемы и в них тепло!</p>
                 </li>
                 <li>
+                    <div class="abt-icon abt-icon-2"></div>
+                    <p>На территории базы имеется детская игровая зона.</p>
+                </li>
+                <li>
                     <div class="abt-icon abt-icon-5"></div>
-                    <p>В каждом домике есть большой зал для застолья, кухня, бытовые кухонные приборы, душевая кабина</p>
+                    <p>В каждом домике есть большой зал для застолья, кухня, бытовые кухонные приборы, душевая
+                        кабина</p>
+                </li>
+                <li>
+                    <div class="abt-icon abt-icon-3"></div>
+                    <p>А также на нашей базе соверешенно бесплатная охраняемая стоянка.</p>
                 </li>
                 <li>
                     <div class="abt-icon abt-icon-6"></div>
-                    <p>В каждом домике есть большой зал для застолья, кухня, бытовые кухонные приборы, душевая кабина</p>
+                    <p>На территории базы имеется баня и переносные мангалы!</p>
                 </li>
             </ul>
         </div>
+        <div class="anchor"></div>
     </article>
-    <article class="in_scrollspy centrator"id="test_3">
+    <article class="in_scrollspy centrator" id="test_3">
         <div class="centerator">
-            <div class="picterform"></div>
+            <div class="picterform">
+                <form class="ask-form">
+                    <h2 class="d-green">Появился вопрос? Задай!</h2>
+                    <input class="input-name" placeholder="Введите ваше имя" type="text"/>
+
+                    <div class="input-phone">
+                        <input placeholder="Телефон" type="text"/>
+                    </div><!--
+                    --><div class="input-mail">
+                        <input placeholder="E-mail" type="text"/>
+                    </div>
+                    <button class="ask-btn"></button>
+                </form>
+            </div>
         </div>
+        <div class="anchor"></div>
     </article>
     <article class="in_scrollspy centrator" id="test_4">
         <div class="nadstroika">
@@ -81,6 +103,7 @@
                 </div>
             </div>
         </div>
+        <div class="anchor"></div>
     </article>
     <article class="in_scrollspy centrator" id="test_5">
 
@@ -94,21 +117,21 @@
 
 <script type="text/javascript">
     var codropsEvents = <?php echo $datapicks; ?>;
-    $(function() {
+    $(function () {
 
         var cal = $('#calendar').calendario({
-            onDayClick: function($el, $contentEl, dateProperties) {
-                onDayClickUser($el, dateProperties);
-            },
-            caldata: codropsEvents
-        }),
-        $month = $('#custom-month').html(cal.getMonthName()),
-                $year = $('#custom-year').html(cal.getYear());
+                onDayClick: function ($el, $contentEl, dateProperties) {
+                    onDayClickUser($el, dateProperties);
+                },
+                caldata: codropsEvents
+            }),
+            $month = $('#custom-month').html(cal.getMonthName()),
+            $year = $('#custom-year').html(cal.getYear());
 
-        $('#custom-next').on('click', function() {
+        $('#custom-next').on('click', function () {
             cal.gotoNextMonth(updateMonthYear);
         });
-        $('#custom-prev').on('click', function() {
+        $('#custom-prev').on('click', function () {
             cal.gotoPreviousMonth(updateMonthYear);
         });
 
