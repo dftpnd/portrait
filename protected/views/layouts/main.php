dada8c765068c3152a698ca76ee027666655981e
Maks <lkdnvc@gmail.com>
2014-02-02 14:56:44 +0400

    test

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,50 +1,53 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <meta name="language" content="ru"/>
-        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-    </head>
-    <body>
-        <div class="universe">
-            <div class="wrapper">
-                <div class="header fixerus" id="fixed-main-menu">
-                    <div class="centrator">
-                        
-                        <ul class="head-ul">
-                            <li class="logo">
-                                <a href="/#main" id="linklogo">
-                                <div class="logo-img"></div>
-                                <div class="logo-text">
-                                    <h3 class="turquoise">База отдыха <b>Илеть</b></h3>
-                                    <h3 class="d-turquoise">в Марий Эл</h3>
-                                </div>
-                                </a>
-                            </li>
-                            <li>
-                                <nav>
-                                    <?php $this->renderPartial('application.views.layouts._menu'); ?>
-                                </nav>
-                            </li>
-                            <li class="contacts">
-                                <h2>+7 (962) 561-67-39</h2>
-                                <div class="button"></div>
-                            </li>
-                            <li class="helper"></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="after_head" ></div>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="language" content="ru"/>
+    <title>База отдыха Илеть</title>
+</head>
+<body>
+<div class="universe">
+    <div class="wrapper">
+        <div class="header_control"></div>
+        <div class="header fixerus header_control" id="fixed-main-menu">
+            <div class="centrator">
 
+                <ul class="head-ul">
+                    <li class="logo">
+                        <a href="/#main" id="linklogo">
+                            <div class="logo-img"></div>
+                            <div class="logo-text">
+                                <h3 class="turquoise">База отдыха <b>Илеть</b></h3>
 
+                                <h3 class="d-turquoise">в Марий Эл</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <nav>
+                            <?php $this->renderPartial('application.views.layouts._menu'); ?>
+                        </nav>
+                    </li>
+                    <li class="contacts">
+                        <h2>+7 (962) 561-67-39</h2>
 
-                <?php echo $content; ?>
-                <div class="acnhor"></div>
-            </div>
-            <div class="footer centrator">
-
+                        <div class="button"></div>
+                    </li>
+                    <li class="helper"></li>
+                </ul>
             </div>
         </div>
+        <div class="after_head"></div>
 
-    </body>
+
+
+        <?php echo $content; ?>
+        <div class="acnhor"></div>
+    </div>
+    <div class="footer centrator">
+
+    </div>
+</div>
+
+</body>
 </html>
