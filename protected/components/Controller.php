fb5cd6035c1506e876cb64473e00f2996d3c5e56
gryaznov M.V <lkdnvc@gmail.com>
2014-02-18 17:51:57 +0400

    admin restore

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -2,33 +2,23 @@
 
 class Controller extends CController
 {
-    public  $breadcrumbs = '';
+    public $breadcrumbs = '';
 
     public function init()
     {
         $cs = Yii::app()->clientScript;
         $cs->registerCoreScript('jquery');
 
-
         $cs->registerCssFile($this->createUrl('/css/bootstrap.min.css'));
         $cs->registerCssFile($this->createUrl('/css/modal/bootstrap-modal.css'));
         $cs->registerCssFile($this->createUrl('/css/modal/bootstrap-modal-bs3patch.css'));
 
-
-
         $cs->registerCssFile($this->createUrl('/css/main.css'));
 
-
-
-        $cs->registerScriptFile($this->createUrl('/js/carousel.js'));
-        $cs->registerScriptFile($this->createUrl('/js/prettyPhoto.js'));
-
-
         $cs->registerScriptFile($this->createUrl('/js/action.js'));
-        $cs->registerScriptFile('/js/bootstrap.min.js');
-        $cs->registerScriptFile('/js/modal/bootstrap-modal.js');
-        $cs->registerScriptFile('/js/modal/bootstrap-modalmanager.js');
-
+        $cs->registerScriptFile($this->createUrl('/js/bootstrap.min.js'));
+        $cs->registerScriptFile($this->createUrl('/js/modal/bootstrap-modal.js'));
+        $cs->registerScriptFile($this->createUrl('/js/modal/bootstrap-modalmanager.js'));
 
 
     }
