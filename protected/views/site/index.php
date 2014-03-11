d83be0236ffceabd6701c6d835e8601a379732d1
Maks Gryaznov <lkdnvc@gmail.com>
2014-03-11 20:23:22 +0400

    asd

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,14 +12,14 @@
 
         <div class="act-price">
             <div>
-                <span class="active">2000 р</span>
+                <span class="active">4000 р</span>
                 <!--                <span class="no-price">вместо</span>-->
 
 <!--                <div class="no-price hr"><hr>4000p</div>-->
                 <span class="no-price">в будни</span>
             </div>
             <div>
-                <span class="active">3000 р</span>
+                <span class="active">6000 р</span>
                 <!--                <span class="no-price">вместо</span>-->
                 <!--                <span class="no-price hr"><hr>6000p</span>-->
                 <span class="no-price">в выходные</span>
